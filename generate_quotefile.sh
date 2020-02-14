23489d67063c0f28d44fc3283d7efd7fde3a9bf5
Vinícius Alves Hax <viniciushax@gmail.com>
2020-02-14 12:14:48 -0300

    * Changing directory structure do add politics quotes * Creating directories for politics and politics-BR quotes * Changing script to generate quotefiles in new structures

--- generate_quotefile.sh
+++ generate_quotefile.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
-strfile entrepreneurship entrepreneurship.dat
-strfile entrepreneurship.ptBR entrepreneurship.ptBR.dat
+#TODO Check if strfile is available
+strfile entrepreneurship/entrepreneurship entrepreneurship/entrepreneurship.dat
+strfile entrepreneurship-ptBR/entrepreneurship-ptBR entrepreneurship-ptBR/entrepreneurship-ptBR.dat
+strfile politics/politics politics/politics.dat
+strfile politics-ptBR/politics-ptBR politics-ptBR/politics-ptBR.dat
